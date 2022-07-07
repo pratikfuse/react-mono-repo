0786db4b79363ceab27391e4775b3ef129e0d0e2
pratikm <pratik.manandhar99@gmail.com>
2022-07-07 13:30:22 +0545

    add button unit test

--- packages/components/src/Button/Button.spec.tsx
+++ packages/components/src/Button/Button.spec.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Button } from "./button";
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 
 describe("Button Component", () => {
   afterEach(() => cleanup());
-
   it("renders a button", () => {
-    const { queryByRole } = render(<Button onClick={() => {}}>Click</Button>);
+    const { queryByRole, queryByText } = render(
+      <Button onClick={() => {}}>Click</Button>
+    );
     expect(queryByRole("button")).toBeTruthy();
+    expect(queryByText("Click")).toBeTruthy();
+  });
+
+  it("handles button clicks", () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>Click</Button>);
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(onClick).toBeCalled();
   });
 });
