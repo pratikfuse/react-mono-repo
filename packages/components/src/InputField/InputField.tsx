b44696f49d7aa6d2308137cf24300b3261959f62
pratikm <pratik.manandhar99@gmail.com>
2022-07-11 15:10:25 +0545

    feat: creates table component

--- packages/components/src/InputField/InputField.tsx
+++ packages/components/src/InputField/InputField.tsx
@@ -1,11 +1,13 @@
 import classNames from "classnames";
-import React from "react";
+import React, { ChangeEventHandler } from "react";
 
 interface IInputFieldProps {
   wrapperClassname?: string;
   className?: string;
   placeholder?: string;
   label: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  hidden?: boolean;
 }
 
 const InputField: React.FC<IInputFieldProps> = (props) => {
@@ -15,7 +17,12 @@ const InputField: React.FC<IInputFieldProps> = (props) => {
   return (
     <div className={wrapperClassname}>
       {props.label && <label>{props.label}</label>}
-      <input className={inputClassname} placeholder={props.placeholder} />
+      <input
+        className={inputClassname}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        hidden={props.hidden}
+      />
     </div>
   );
 };
---
+++ packages/components/src/Table/table.tsx
@@ -0,0 +1,47 @@
+import classNames from "classnames";
+import React from "react";
+import { useTable, TableOptions, usePagination } from "react-table";
+/**
+ * TODO add virtualized list -> react-window
+ *
+ */
+
+interface ITableProps<T extends Object> extends TableOptions<T> {
+  paginate?: boolean;
+  wrapperClassName?: string;
+  fetchData: ({}: { pageSize: number; pageNumber?: number }) => void;
+}
+
+export const Table: React.FC<ITableProps<{}>> = (props) => {
+  const wrapperClassName = classNames(props.wrapperClassName);
+  const tableClassName = classNames("table");
+  const {
+    getTableProps,
+    headerGroups,
+    getTableBodyProps,
+    state: {},
+  } = useTable(
+    {
+      columns: props.columns,
+      data: props.data,
+    },
+    usePagination
+  );
+
+  return (
+    <div className={wrapperClassName}>
+      <table className={tableClassName} {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>{}</tbody>
+      </table>
+    </div>
+  );
+};
