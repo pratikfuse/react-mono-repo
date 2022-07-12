027aa873c8aad6cfd7218249d5952d4a947cca2e
pratikm <pratik.manandhar99@gmail.com>
2022-07-12 11:30:31 +0545

    fixed repo package name

--- apps/web/src/Auth/pages/Login/index.tsx
+++ apps/web/src/Auth/pages/Login/index.tsx
@@ -1,24 +1,27 @@
-import Button from "components/lib/Button";
-import { useAppDispatch } from "src/Common/redux/hooks";
-import InputField from "components/lib/InputField";
-import { useState } from "react";
-import actions from "src/Auth/redux/actions";
-import { useNavigate } from "react-router-dom";
+import Button from '@lf-mono-web/components/lib/Button';
+import { useAppDispatch } from 'src/Common/redux/hooks';
+import InputField from '@lf-mono-web/components/lib/InputField';
+import { useState } from 'react';
+import actions from 'src/Auth/redux/actions';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage: React.FC = () => {
   const dispatch = useAppDispatch();
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
   const navigate = useNavigate();
 
   return (
     <div>
       Login Page
-      <InputField label="email" onChange={(e) => setEmail(e.target.value)} />
+      <InputField
+        label="email"
+        onChange={e => setEmail(e.target.value)}
+      />
       <InputField
         label="password"
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={e => setPassword(e.target.value)}
       />
       <Button
         onClick={() => {
@@ -27,7 +30,7 @@ const LoginPage: React.FC = () => {
               email,
               password,
               navigate, // pass navigate to action to navigate to the app route on successful authentication
-            })
+            }),
           );
         }}
       >
