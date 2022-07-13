d99e2ce92bf9ec58e40ecd2ff160d17889fdb6a8
pratikm <pratik.manandhar99@gmail.com>
2022-07-14 00:28:16 +0545

    add thunk action

--- apps/web/src/Auth/pages/Login/index.tsx
+++ apps/web/src/Auth/pages/Login/index.tsx
@@ -1,17 +1,53 @@
-// import Button from '@lf-mono-web/components/lib/Button';
-// import { useAppDispatch } from 'src/Common/redux/hooks';
-// import InputField from '@lf-mono-web/components/lib/InputField';
-// import { useState } from 'react';
-// import actions from 'src/Auth/redux/actions';
-// import { useNavigate } from 'react-router-dom';
-
-// import { useEffect, useState } from 'react';
-// import { useAppDispatch } from 'src/Common/redux/hooks';
+import { useState } from 'react';
+import Button from '@lf-mono-web/components/lib/Button';
+import { useAppDispatch } from 'src/Common/redux/hooks';
+import InputField from '@lf-mono-web/components/lib/InputField';
+import { login } from 'src/Auth/redux/reducer';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage: React.FC = () => {
-  // const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-  return <div>Login Page</div>;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string>('');
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    const response = await dispatch(
+      login({ email, password }),
+    );
+    if (response.meta.requestStatus === 'rejected') {
+      alert('not valid');
+    } else if (
+      response.meta.requestStatus === 'fulfilled'
+    ) {
+      navigate('/');
+    }
+  };
+
+  return (
+    <div>
+      <h4>Login </h4>
+      <small>{error}</small>
+      <InputField
+        onChange={e => {
+          setEmail(e.target.value);
+
+          if (e.target.value) {
+            setError('');
+          }
+        }}
+        label="email"
+      />
+      <InputField
+        onChange={e => setPassword(e.target.value)}
+        label="password"
+      />
+
+      <Button onClick={handleLogin}>Login</Button>
+    </div>
+  );
 };
 
 export default LoginPage;
--- apps/web/src/Auth/redux/reducer.ts
+++ apps/web/src/Auth/redux/reducer.ts
@@ -1,41 +1,80 @@
 import {
+  createAsyncThunk,
   createSlice,
   DeepPartial,
-  PayloadAction,
 } from '@reduxjs/toolkit';
-import { NavigateFunction } from 'react-router-dom';
-
+import {
+  AppDispatch,
+  RootState,
+} from 'src/Common/redux/store';
+import authService from '../services/authService';
+import {
+  LoginRequest,
+  LoginResponse,
+} from '../services/authService.types';
 export interface AuthState {
   isLoggedIn: boolean;
   user: DeepPartial<IUser>;
+  loading: boolean;
 }
 
 const initialState: AuthState = {
   isLoggedIn: true,
   user: {},
+  loading: false,
 };
+
+export const login = createAsyncThunk<
+  LoginResponse,
+  LoginRequest,
+  {
+    // Optional fields for defining thunkApi field types
+    dispatch: AppDispatch;
+    state: RootState;
+    rejectValue: ErrorResponse;
+  }
+>('auth/login', async ({ email, password }, thunk) => {
+  try {
+    const response = await authService.login({
+      email,
+      password,
+    });
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      if (!response.data) {
+        return thunk.rejectWithValue({
+          error: 'not valid',
+        } as ErrorResponse);
+      }
+      return thunk.rejectWithValue({
+        error: 'not valid',
+      } as ErrorResponse);
+    }
+  } catch (error: any) {
+    return thunk.rejectWithValue({
+      error: 'not valid',
+    } as ErrorResponse);
+  }
+});
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {
-    logIn: (
-      state,
-      action: PayloadAction<{
-        email: string;
-        password: string;
-        navigate: NavigateFunction;
-      }>,
-    ) => {
-      if (
-        action.payload.email === 'admin' &&
-        action.payload.password === 'admin'
-      ) {
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(login.pending, state => {
+        state.loading = true;
+      })
+      .addCase(login.fulfilled, state => {
+        state.loading = false;
         state.isLoggedIn = true;
-        action.payload.navigate('/app');
-      }
-    },
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.isLoggedIn = false;
+      });
   },
 });
-
 export default authSlice.reducer;
