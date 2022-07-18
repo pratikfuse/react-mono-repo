7f1d1af618811c8a1b434ffb176a91f8ac2e518c
pratikm <pratik.manandhar99@gmail.com>
2022-07-18 11:52:56 +0545

    feat: added synthetic login api delay

--- src/apps/server/middlewares/login.js
+++ src/apps/server/middlewares/login.js
@@ -2,12 +2,15 @@
 module.exports = (req, res, next) => {
     if (req.method === 'POST' && req.path === '/auth/login') {
         if (req.body.email === 'user1@admin.com' && req.body.password === 'admin') {
-            res.status(200).json({
-                data: {
-                    access_token: 'access_token',
-                    refresh_token: 'refresh_token',
-                },
-            });
+            setTimeout(() => {
+                res.status(200).json({
+                    data: {
+                        access_token: 'access_token',
+                        refresh_token: 'refresh_token',
+                    },
+                });
+            }, 3000);
+
         } else {
 
             res.status(400).json({
